fa38f99548b6a8b3916b07ac806064bb82f04f21
Shivam Balwani <48817020+balwanishivam@users.noreply.github.com>
2020-05-07 23:51:28 +0530

    forms added (#17)
    * added foreign key for MyUser in all 4 models (no change previously
    
    * Models Modified
    
    * forms pr (#16)
    
    * Models Modified (#15)
    
    * added foreign key for MyUser in all 4 models (no change previously
    
    * Models Modified
    
    Co-authored-by: Swasti Khurana <swastikhurana0800@gmail.com>
    
    * Update README.md
    
    * M_S and BB forms added
    
    Co-authored-by: Shivam Balwani <48817020+balwanishivam@users.noreply.github.com>
    
    Co-authored-by: Swasti Khurana <swastikhurana0800@gmail.com>
    Co-authored-by: SwastiKh <42692321+SwastiKh@users.noreply.github.com>

---
+++ Medical_Store/forms.py
@@ -0,0 +1,15 @@
+from django.contrib.auth.models import Medical_Store
+from django import forms
+from myuser.models import Myuser
+
+class RegistrationForm(forms.ModelForm):
+    password=forms.CharField(widget=forms.PasswordInput)
+    class Meta:
+        model = Myuser
+        fields=['email','password','organisation_name','user_type']
+
+class LoginForm(forms.ModelForm):
+    password=forms.CharField(widget=forms.PasswordInput)
+    class Meta:
+        model=Myuser
+        fields=['email','password','user_type']
