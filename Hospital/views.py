3feb5d4159e8836f8483ff1e92106cadc1a3e5b9
Shivam Balwani <48817020+balwanishivam@users.noreply.github.com>
2020-05-29 19:47:24 +0530

    Hospital Views Created

--- Hospital/views.py
+++ Hospital/views.py
@@ -35,6 +35,8 @@ class Index(View):
     def get(self,request):
         return render(request,self.template_name)
 
+
+#Doctor's Views
 #Add Doctor's details
 class AddDoctor(LoginRequiredMixin,View):
     form_class=DoctorDetails
@@ -54,10 +56,33 @@ class AddDoctor(LoginRequiredMixin,View):
         
         return render(request,self.template_name,{'form':form})
 
+#View Doctor
+class ViewDoctor(ListView):
+    template_name='Hospital/doctor.html'
+    context_object_name='doctors'
+    model=Doctor
+    def get_queryset(self):
+        qs = super().get_queryset()
+        user = self.request.user
+        if user is None:
+            return qs
+        return qs.filter(user=user)
+
+#Edit Doctor(still to be updated)
+class EditDoctor(UpdateView):
+    model=Doctor
+    form_class=DoctorDetails
+
+#Delete Doctor
+class DeleteDoctor(DeleteView):
+    model=Doctor
+    success_url=reverse_lazy('Hospital:view_doctor')
+
+#Patient Record
 # Add Patient's Records
 class AddPatient(LoginRequiredMixin,View):
     form_class=PatientDetails
-    template_name='Hospital/add_patient.html'
+    template_name='Hospital/patient_form.html'
 
     def get(self,request):
         form=self.form_class(None)
@@ -73,10 +98,33 @@ class AddPatient(LoginRequiredMixin,View):
         
         return render(request,self.template_name,{'form':form})
 
+#View Patient
+class ViewPatient(ListView):
+    template_name='Hospital/patient.html'
+    context_object_name='patients'
+    model=Patient
+    def get_queryset(self):
+        qs = super().get_queryset()
+        user = self.request.user
+        if user is None:
+            return qs
+        return qs.filter(user=user)
+
+#Edit Patient(still to be updated)
+class EditPatient(UpdateView):
+    model=Patient
+    form_class=PatientDetails
+
+#Delete Patient
+class Deletepatient(DeleteView):
+    model=Patient
+    success_url=reverse_lazy('Hospital:view_patient')
+
+# Test Available
 # Test details
 class AddTest(LoginRequiredMixin,View):
     form_class=TestDetails
-    template_name='Hospital/add_test.html'
+    template_name='Hospital/test_form.html'
 
     def get(self,request):
         form=self.form_class(None)
@@ -92,10 +140,34 @@ class AddTest(LoginRequiredMixin,View):
         
         return render(request,self.template_name,{'form':form})
 
+#View Tests
+class ViewTest(ListView):
+    template_name='Hospital/test.html'
+    context_object_name='tests'
+    model=Tests
+    def get_queryset(self):
+        qs = super().get_queryset()
+        user = self.request.user
+        if user is None:
+            return qs
+        return qs.filter(user=user)
+
+#Edit Tests(still to be updated)
+class EditTest(UpdateView):
+    model=Tests
+    form_class=TestDetails
+
+#Delete Tests
+class DeleteTest(DeleteView):
+    model=Tests
+    success_url=reverse_lazy('Hospital:view_test')
+
+
+#Test Conducted
 # Patient Test Conducted 
 class PatientTest(LoginRequiredMixin,View):
     form_class=TestConducted
-    template_name='Hospital/add_conducted.html'
+    template_name='Hospital/conducted_form.html'
 
     def get(self,request):
         form=self.form_class(None)
@@ -111,10 +183,11 @@ class PatientTest(LoginRequiredMixin,View):
         
         return render(request,self.template_name,{'form':form})
 
-class ViewDoctor(ListView):
-    template_name='Hospital/doctor.html'
-    context_object_name='doctors'
-    model=Doctor
+#View Conducted
+class ViewConducted(ListView):
+    template_name='Hospital/conducted.html'
+    context_object_name='conducted'
+    model=Conducted
     def get_queryset(self):
         qs = super().get_queryset()
         user = self.request.user
@@ -122,12 +195,14 @@ class ViewDoctor(ListView):
             return qs
         return qs.filter(user=user)
 
-class EditDoctor(UpdateView):
-    model=Doctor
-    form_class=DoctorDetails
+#Edit Conducted(still to be updated)
+class EditConducted(UpdateView):
+    model=Conducted
+    form_class=TestConducted
 
-class DeleteDoctor(DeleteView):
-    model=Doctor
-    success_url=reverse_lazy('Hospital:view_doctor')
+#Delete Conducted
+class DeleteConducted(DeleteView):
+    model=Conducted
+    success_url=reverse_lazy('Hospital:view_conducted')
 
 # Create your views here.
